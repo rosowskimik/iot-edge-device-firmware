0d1847dc6baecdedc2d7e53b063f749e20f4f921
Mikołaj Rosowski <m.rosowski1@wp.pl>
2025-12-17 11:51:47 +0100

    add sensor definitions
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- src/sensor_map.h
+++ src/sensor_map.h
@@ -18,14 +18,23 @@
 	SENSOR_CHAN_BY_COMPAT(phandle, maxim_ds18b20, {SENSOR_CHAN_AMBIENT_TEMP, 0})               \
 	SENSOR_CHAN_BY_COMPAT(phandle, aosong_dht, {SENSOR_CHAN_AMBIENT_TEMP, 0},                  \
 			      {SENSOR_CHAN_HUMIDITY, 0})                                           \
+	SENSOR_CHAN_BY_COMPAT(phandle, aosong_dht20, {SENSOR_CHAN_AMBIENT_TEMP, 0},                \
+			      {SENSOR_CHAN_HUMIDITY, 0})                                           \
 	SENSOR_CHAN_BY_COMPAT(phandle, bosch_bmp180, {SENSOR_CHAN_PRESS, 0},                       \
 			      {SENSOR_CHAN_DIE_TEMP, 0})                                           \
+	SENSOR_CHAN_BY_COMPAT(phandle, bosch_bme280, {SENSOR_CHAN_AMBIENT_TEMP, 0},                \
+			      {SENSOR_CHAN_PRESS, 0}, {SENSOR_CHAN_HUMIDITY, 0})                   \
+	SENSOR_CHAN_BY_COMPAT(phandle, bosch_bmp280, {SENSOR_CHAN_AMBIENT_TEMP, 0},                \
+			      {SENSOR_CHAN_PRESS, 0})                                              \
 	SENSOR_CHAN_BY_COMPAT(phandle, bosch_bme680, {SENSOR_CHAN_AMBIENT_TEMP, 0},                \
-			      {SENSOR_CHAN_PRESS, 0}, {SENSOR_CHAN_HUMIDITY, 0},                   \
-			      {SENSOR_CHAN_GAS_RES, 0})                                            \
+			      {SENSOR_CHAN_PRESS, 0}, {SENSOR_CHAN_HUMIDITY, 0})                   \
 	SENSOR_CHAN_BY_COMPAT(phandle, avago_apds9960, {SENSOR_CHAN_LIGHT, 0},                     \
 			      {SENSOR_CHAN_RED, 0}, {SENSOR_CHAN_GREEN, 0},                        \
-			      {SENSOR_CHAN_GAS_BLUE, 0}, {SENSOR_CHAN_PROX, 0})
+			      {SENSOR_CHAN_GAS_BLUE, 0}, {SENSOR_CHAN_PROX, 0})                    \
+	SENSOR_CHAN_BY_COMPAT(phandle, vishay_vcnl4040, {SENSOR_CHAN_PROX, 0},                     \
+			      {SENSOR_CHAN_LIGHT, 0})                                              \
+	SENSOR_CHAN_BY_COMPAT(phandle, aosong_dht, {SENSOR_CHAN_AMBIENT_TEMP, 0},                  \
+			      {SENSOR_CHAN_HUMIDITY, 0})
 
 #define SENSOR_CHAN_COUNT(node_id, prop, idx)                                                      \
 	ARRAY_SIZE(((struct sensor_chan_spec[]){                                                   \
