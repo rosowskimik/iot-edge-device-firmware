6caf39b76ee144f4a8a9a05f5eeab8e7de934733
Mikołaj Rosowski <m.rosowski1@wp.pl>
2026-01-11 20:40:55 +0100

    net: use static ip address
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- src/net.c
+++ src/net.c
@@ -1,5 +1,6 @@
 #include "net.h"
 #include "storage.h"
+#include "zephyr/net/net_event.h"
 
 #include <errno.h>
 #include <stdint.h>
@@ -30,38 +31,20 @@ static void l4_event_handler(uint64_t mgmt_event, struct net_if *iface, void *in
 	ARG_UNUSED(user_data);
 
 	switch (mgmt_event) {
-	case NET_EVENT_DNS_SERVER_ADD:
-		LOG_DBG("network connected");
+	case NET_EVENT_L4_CONNECTED:
+		LOG_INF("network connected");
 		k_sem_give(&network_connected);
 		break;
 	case NET_EVENT_L4_DISCONNECTED:
-		LOG_DBG("network disconnected");
+		LOG_INF("network disconnected");
 		k_sem_take(&network_connected, K_NO_WAIT);
 		break;
 	}
 }
 
 static NET_MGMT_REGISTER_EVENT_HANDLER(net_connection_mgmt_handler,
-				       NET_EVENT_DNS_SERVER_ADD | NET_EVENT_L4_DISCONNECTED,
+				       NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED,
 				       l4_event_handler, NULL);
-
-static int dns_lookup_server(struct zsock_addrinfo **addr)
-{
-	int rc;
-
-	struct zsock_addrinfo hints = {
-		.ai_family = AF_INET,
-		.ai_socktype = SOCK_STREAM,
-	};
-
-	rc = zsock_getaddrinfo(CONFIG_APP_SERVER_HOSTNAME, NULL, &hints, addr);
-	if (rc) {
-		LOG_ERR("failed to resolve '" CONFIG_APP_SERVER_HOSTNAME "' hostname (err %d)", rc);
-		return rc;
-	}
-
-	return 0;
-}
 
 static void net_disconnect()
 {
@@ -99,6 +82,8 @@ static int net_connect()
 	params.band = WIFI_FREQ_BAND_2_4_GHZ;
 	params.mfp = WIFI_MFP_OPTIONAL;
 
+	LOG_DBG("Connecting to network %s", ssid);
+
 	rc = net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &params,
 		      sizeof(struct wifi_connect_req_params));
 	if (rc) {
@@ -108,6 +93,7 @@ static int net_connect()
 
 	rc = k_sem_take(&network_connected, K_SECONDS(CONFIG_APP_NETWORK_TIMEOUT));
 	if (rc) {
+		LOG_ERR("Failed to connect to network (err %d)", rc);
 		net_disconnect();
 	}
 
@@ -118,8 +104,7 @@ int server_connect()
 {
 	int rc;
 	int sock;
-	struct zsock_addrinfo *addr;
-	struct sockaddr_in *sa;
+	struct sockaddr_in sa;
 
 	rc = net_connect();
 	if (rc) {
@@ -127,9 +112,14 @@ int server_connect()
 	}
 	k_sleep(K_SECONDS(1));
 
-	rc = dns_lookup_server(&addr);
-	if (rc) {
-		goto _err_net_disconnect;
+	sa = (struct sockaddr_in){
+		.sin_family = AF_INET,
+		.sin_port = htons(CONFIG_APP_SERVER_PORT),
+	};
+
+	rc = zsock_inet_pton(AF_INET, CONFIG_APP_SERVER_IP, &sa.sin_addr);
+	if (rc < 0) {
+		LOG_ERR("invalid ip address format: '%s'", CONFIG_APP_SERVER_IP);
 	}
 
 	rc = zsock_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -139,25 +129,13 @@ int server_connect()
 	}
 	sock = rc;
 
-	for (; addr != NULL; addr = addr->ai_next) {
-		if (addr->ai_addr->sa_family == AF_INET) {
-			sa = (struct sockaddr_in *)addr->ai_addr;
-			sa->sin_port = htons(CONFIG_APP_SERVER_PORT);
-
-			rc = zsock_connect(sock, (struct sockaddr *)sa, sizeof(struct sockaddr_in));
-			if (rc == 0) {
-				LOG_DBG("server connected");
-				return sock;
-			}
-
-			zsock_close(sock);
-
-			if (addr->ai_next) {
-				LOG_WRN("server connection failed, trying next address (err %d)",
-					-errno);
-			}
-		}
+	rc = zsock_connect(sock, (struct sockaddr *)&sa, sizeof(sa));
+	if (rc == 0) {
+		LOG_DBG("server connected");
+		return sock;
 	}
+
+	zsock_close(sock);
 
 	LOG_ERR("server connection failed (err %d)", -errno);
 _err_net_disconnect:
